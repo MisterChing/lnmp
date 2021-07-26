a1cf08980b03a64c238b32ffe1133392410bdf0f
MisterChing <chfnaruto@gmail.com>
2021-07-26 16:24:48 +0800

    update crontab sh

--- bash_script/crontab_second_ctl.sh
+++ bash_script/crontab_second_ctl.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [[ $# -lt 1 ]]; then
-    echo "Usage: bash(not sh) crontab_second.sh {\$step} command..."
+if [[ $# -lt 3 ]]; then
+    echo "Usage: bash(not sh) crontab_second.sh {\$total_second} {\$step} command..."
     exit 0
 fi
+total_s=$1 && shift
 step=$1 && shift
 
 cmd=`printf "%q " "$@"`
@@ -12,28 +13,46 @@ function isBusyBox() {
     echo | busybox md5sum &>/dev/null && echo yes || echo no
 }
 
-function timeMS() {
-    declare -i time_ms
+# microseconds
+function timeUS() {
+    declare -i time_us
     if [[ $(isBusyBox) == "yes" ]]; then
-        time_ms=$(adjtimex | awk '/(time.tv_sec|time.tv_usec):/ { printf("%06d", $2) }')
-    elif [[ "x$(uname)" == "xDarwin" ]]; then
-        time_ms=$(gdate +%s%6N)
+        time_us=$(adjtimex | awk '/(time.tv_sec|time.tv_usec):/ { printf("%-9d", $2) }' | sed 's/ /0/g')  # add trailing zeros
+    elif [[ x"$(uname)" == x"Darwin" ]]; then
+        time_us=$(gdate +%s%6N)
     else
-        time_ms=$(date +%s%6N)
+        time_us=$(date +%s%6N)
     fi
-    echo $time_ms
+    echo $time_us
 }
 
-for (( i = 0; i < 60; i=$(($i+step)) )); do
-    echo ${i}
-    start_ms=$(timeMS)
+dest=$(( `date +%s`+ $total_s-1 ))  # total_s-1: trick reduce code & sys time cost
+while [ `date +%s` -lt $dest ]; do
+    start_us=$(timeUS)
     eval $cmd
-    end_ms=$(timeMS)
-    time_cost=$(($step * 1000000 - ($end_ms - $start_ms)))
-    if [ $time_cost -gt 0 ]; then
-      sleep $(awk "BEGIN{print ($time_cost+0.0)/1000000}")
-      echo "sleep time: $(awk "BEGIN{print ($time_cost+0.0)/1000000}")"
+    end_us=$(timeUS)
+    cost_ms=$(awk "BEGIN{print ($end_us - $start_us)/1000000000.0}")
+    sleep_us=$(( $step * 1000000000 - ($end_us - $start_us) ))
+    if [ $sleep_us -gt 0 ]; then
+      sleep $(awk "BEGIN{print ($sleep_us+0.0)/1000000000}")
+      echo "cost_time: $cost_ms sleep_time: $(awk "BEGIN{print ($sleep_us+0.0)/1000000000}")"
     else
-      echo "no sleep"
+      echo "cost_time: $cost_ms no sleep"
     fi
 done
+
+
+#for (( i = 0; i < $total_s-1; i=$(($i+step)) )); do   # total_s-1: trick reduce code & sys time cost
+#    echo ${i}
+#    start_us=$(timeUS)
+#    eval $cmd
+#    end_us=$(timeUS)
+#    cost_ms=$(awk "BEGIN{print ($end_us - $start_us)/1000000000.0}")
+#    sleep_us=$(( $step * 1000000000 - ($end_us - $start_us) ))
+#    if [ $sleep_us -gt 0 ]; then
+#      sleep $(awk "BEGIN{print ($sleep_us+0.0)/1000000000}")
+#      echo "cost_time: $cost_ms sleep_time: $(awk "BEGIN{print ($sleep_us+0.0)/1000000000}")"
+#    else
+#      echo "cost_time: $cost_ms no sleep"
+#    fi
+#done
