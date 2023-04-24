a4970d6c097c9520bc4012fd443a3e11567f0e10
MisterChing <chfnaruto@gmail.com>
2023-04-24 18:07:46 +0800

    debug

--- bash_script/tcpdump.sh
+++ bash_script/tcpdump.sh
@@ -2,7 +2,9 @@
 host=$1
 port=$2
 if [ "${port}" == "" ]; then
-    tcpdump -i any host ${host} -nn -A
+    #tcpdump -i any host ${host} -nn -A
+    tcpdump -i any host ${host} -tttt -nnv
 else
-    tcpdump -i any host ${host} and port ${port} -nn -A
+    #tcpdump -i any host ${host} and port ${port} -nn -A
+    tcpdump -i any host ${host} and port ${port} -tttt -nnv
 fi
